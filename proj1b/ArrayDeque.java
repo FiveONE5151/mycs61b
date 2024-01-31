d1e08a8b7346159be59103dcf67d74b2cd96f9cb
WuYi <2310199261@qq.com>
2024-01-31 12:03:06 +0800

    project1b finished(adding arrayDeque for compilation)

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,169 @@
+/**
+ * Circular array deque
+ *
+ * @param <T>
+ * @author Jerry-W
+ * @source https://github.com/PKUFlyingPig/CS61B/blob/master/proj1a/ArrayDeque.java (the for loop logic optimized)
+ */
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] array;
+    private int size;
+    private int length = 8;
+    private final double limit = 0.25;
+    private final int RESIZE_FACTOR = 2;
+    /**
+     * Both front and end pointing to the same place means the array is empty (end+1)%length==front means the array is
+     * full
+     */
+    private int front = 0;
+    private int end = 0;
+
+    public ArrayDeque() {
+        array = (T[])new Object[length];
+    }
+
+    public ArrayDeque(int size) {
+        this.size = size;
+        length = size * RESIZE_FACTOR;
+        array = (T[])new Object[length];
+    }
+
+    public ArrayDeque(ArrayDeque other) {
+
+    }
+
+    private void resize() {
+        if (((double)(size - 1) / length) < limit) {
+            T[] newArray = (T[])new Object[length / RESIZE_FACTOR];
+            arrayCopy((T[])newArray);
+            length /= RESIZE_FACTOR;
+            array = newArray;
+        } else if (size + 1 >= length) {
+            T[] newArray = (T[])new Object[(size + 1) * RESIZE_FACTOR];
+            arrayCopy((T[])newArray);
+            length = (size + 1) * RESIZE_FACTOR;
+            array = newArray;
+        }
+    }
+
+    /**
+     * customized minusOne for the circular array
+     *
+     * @param index index
+     * @return the actual index after minusOne
+     */
+    private int minusOne(int index) {
+        if (index == 0)
+            return length - 1;
+        return index - 1;
+    }
+
+    /**
+     * customized plusOne for the circular array
+     *
+     * @param index  index
+     * @param module the length of the array
+     * @return the actual index after plusOne
+     */
+    private int plusOne(int index, int module) {
+        return (index + 1) % module;
+    }
+
+    /**
+     * optimized for loop
+     *
+     * @param newArray new array
+     * @source https://github.com/PKUFlyingPig/CS61B/blob/master/proj1a/ArrayDeque.java
+     */
+    private void arrayCopy(T[] newArray) {
+        int ptr1 = plusOne(front, length);
+        int ptr2 = 1;
+        do {
+            newArray[ptr2] = array[ptr1];
+            ptr1 = plusOne(ptr1, length);
+            ptr2 = plusOne(ptr2, newArray.length);
+        } while (ptr1 != end);
+        newArray[ptr2] = array[ptr1];
+        front = 0;
+        end = size;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        if (item == null)
+            return;
+        if (size + 1 >= length)
+            resize();
+        ++size;
+        array[front] = item;
+        front = minusOne(front);
+    }
+    @Override
+    public void addLast(T item) {
+        if (item == null)
+            return;
+        if (size + 1 >= length)
+            resize();
+        ++size;
+        end = plusOne(end, length);
+        array[end] = item;
+    }
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public int length() {
+        return length - 1;
+    }
+
+    /**
+     * print the elements of deque
+     *
+     * @source https://github.com/PKUFlyingPig/CS61B/blob/master/proj1a/ArrayDeque.java (optimized for loop)
+     */
+    @Override
+    public void printDeque() {
+        if (size == 0)
+            return;
+        int i = 0;
+        for (i = plusOne(front, length); i != end; i = plusOne(i,
+            length)) { // The loop termination condition is not necessarily a greater than or less than relation
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(array[i]);
+    }
+    @Override
+    public T removeFirst() {
+        if (isEmpty())
+            return null;
+        if ((double)(size - 1) / length < limit && length > 8)// length>8 can avoid the arrayCopy bound issue
+            resize();
+        front = plusOne(front, length);
+        --size;
+        return array[front];
+    }
+    @Override
+    public T removeLast() {
+        if (isEmpty())
+            return null;
+        if ((double)(size - 1) / length < limit && length > 8)// length>8 can avoid the arrayCopy bound issue
+            resize();
+        int temp = end;
+        --size;
+        end = minusOne(end);
+        return array[temp];
+    }
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            String message = String.format("Index %d out of bounds length %d", index, size);
+            throw new IndexOutOfBoundsException(message);
+        }
+        return array[plusOne(index + front, length)];
+    }
+}
