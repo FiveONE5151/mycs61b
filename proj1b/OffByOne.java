bd254e5a8fffee7c9cc6194979b7ddd492034b1c
WuYi <2310199261@qq.com>
2024-01-31 17:14:48 +0800

    project1b finished(adding arrayDeque for compilation) (check the character is alphabetical before comparing them) (dont check alphabetical)

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,15 +1,6 @@
 public class OffByOne implements CharacterComparator {
-
-    private boolean isAlphabetic(char c) {
-        if (c >= 'A' && c <= 'z')
-            return true;
-        return false;
-    }
-
     @Override
     public boolean equalChars(char x, char y) {
-        if (!(isAlphabetic(x) && isAlphabetic(y)))
-            return false;
         if (Math.abs(x - y) == 1)
             return true;
         return false;
